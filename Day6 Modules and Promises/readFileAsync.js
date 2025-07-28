d78cc0ab421df43b3cc0d9ef4e0381edd95b3e08
Meryemsn01 <meryemsouaini01@gmail.com>
2025-07-28 17:57:40 +0100

    implement async file read/write examples
    Introduced readFileAsync.js with commented examples of reading and writing files asynchronously using Promises. Added text.txt as a sample file for demonstration.

---
+++ Day6 Modules and Promises/readFileAsync.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+
+function readFileAsync(path){
+    fs.readFile(path, (err, data)=>{
+        myPromise = new Promise(function(resolve, reject) {
+            if(!err) resolve('OK!')
+            else reject(err)
+        })
+        myPromise
+        .then (() => console.log(data.toString()))
+        .catch(() => console.log(err) )
+    })
+}
+
+readFileAsync('text.txt')
+
+
+
+function writeFileAsync(path , content){
+    fs.appendFile(path, content ,(err)=>{
+        const myPromise =  new Promise((resolve, reject) => {
+            if(err) reject(err)
+            else resolve('ok')
+        })
+         myPromise
+        .then(() =>console.log('content written succesfully'))
+        .catch(()=>console.log(err))
+    })
+}
+
+writeFileAsync('text.txt', '  new content')
+
+function processFiles(){
+    
+}
