8a57e7fe5e44bc9a4e0491ce5a5f7cc8677dfc51
Meryemsn01 <meryemsouaini01@gmail.com>
2025-09-23 12:33:53 +0100

    Initialize React blog landing page with Vite
    Set up a new React project using Vite, including initial configuration files, ESLint setup, and basic component structure for a blog landing page. Added .gitignore, README, and all necessary source and public files.

---
+++ Day40 - React/blog-landing-page/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <h3>Copyright MeryemSOUAINI</h3>
+        </div>
+    )
+}
+export default Footer;
---
+++ Day40 - React/blog-landing-page/src/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent(){
+    return(
+        <div>
+            <h1>Welcome to my blog</h1>
+            <p>this blog contains some information about me </p>
+        </div>
+    )
+}
+export default MainContent;
